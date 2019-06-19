a394214bbe6e23ca375fc215dc393426aa260218
Baghdad <baghdadalkhatib@gmail.com>
2019-06-19 14:47:31 +0300

    put values on LMDB map

--- build.sbt
+++ build.sbt
@@ -4,20 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
-libraryDependencies += "org.lmdbjava" % "lmdbjava" % "0.7.0"
+libraryDependencies += "org.lmdbjava" % "lmdbjava" % "0.6.3"
 
+libraryDependencies += "org.scalaz" %% "scalaz-zio" % "1.0-RC1"
 
-libraryDependencies += "org.agrona" % "Agrona" % "0.9.1"
-
-
-// https://mvnrepository.com/artifact/org.hamcrest/hamcrest-all
-libraryDependencies += "org.hamcrest" % "hamcrest-all" % "1.3" % Test
-
-// https://mvnrepository.com/artifact/org.hamcrest/java-hamcrest
-//libraryDependencies += "org.hamcrest" % "java-hamcrest" % "2.0.0.0" % Test ... not works
-
-//libraryDependencies += "org.hamcrest" % "hamcrest-junit" % "2.0.0.0" % Test ... not works
-
-libraryDependencies += "org.lmdbjava" % "lmdbjava-native-windows-x86_64" % "0.9.23-1"
-// https://mvnrepository.com/artifact/junit/junit
-libraryDependencies += "junit" % "junit" % "4.12" % Test
--- src/main/scala/LMDBMain.scala
+++ src/main/scala/LMDBMain.scala
@@ -1,9 +1,11 @@
 import java.io.File
+import java.nio.ByteBuffer
 import java.nio.ByteBuffer.allocateDirect
 import java.nio.charset.StandardCharsets.UTF_8
+
 import org.lmdbjava.DbiFlags.MDB_CREATE
 import org.lmdbjava.Env.create
-
+import org.lmdbjava.EnvFlags.MDB_NOSUBDIR
 
 object LMDBMain {
   val DB_NAME = "my DB"
@@ -11,25 +13,43 @@ object LMDBMain {
     val obj = new LMDBTest2
     obj.test()
   }
+
 }
 class LMDBTest2 {
   def test () {
-    val file = new File("C:\\Users\\Lenovo\\Desktop\\NEws\\Mar\\LMDB\\LMDBProject\\tempFolder\\test.txt")
-    val env = create.setMapSize(1024*1024).setMaxDbs(1).open(file)
+    val file = new File("C:\\Users\\Lenovo\\Desktop\\Temp\\test.txt")
+    val env2 = create().setMapSize(10485760).setMaxDbs(1)
+    val env = env2.open(file,MDB_NOSUBDIR)
     val db = env.openDbi(LMDBMain.DB_NAME, MDB_CREATE)
+
     val key = allocateDirect(env.getMaxKeySize)
-    val `val` = allocateDirect(700)
-    key.put("greeting".getBytes(UTF_8)).flip
-    `val`.put("Hello world".getBytes(UTF_8)).flip
-    val valSize = `val`.remaining
-    db.put(key, `val`)
+    val value = allocateDirect(700)
+
+    val key2 = allocateDirect(env.getMaxKeySize)
+    val value2= allocateDirect(700)
+    key.put("Greeting".getBytes(UTF_8)).flip
+    value.put("Hello World".getBytes(UTF_8)).flip
+    db.put(key, value)
+
+    key2.put("Greeting2".getBytes(UTF_8)).flip
+    value2.put("Hello World2".getBytes(UTF_8)).flip
+    db.put(key2, value2)
+    val txn = env.txnRead
     try {
-      val txn = env.txnRead
-      try {
-        val found = db.get(txn, key)
-        val fetchedVal = txn.`val`
-      } finally if (txn != null) txn.close()
-    }
+      val found : ByteBuffer  = db.get(txn,key)
+      val fetchedVal : ByteBuffer  = txn.`val`()
+      println(UTF_8.decode(fetchedVal).toString())
+      txn.commit()
+    } finally if (txn != null) txn.close()
+
+    val txn2 = env.txnRead
+    try {
+      val found2 : ByteBuffer  = db.get(txn2,key2)
+      val fetchedVal2 : ByteBuffer  = txn2.`val`()
+      println(UTF_8.decode(fetchedVal2).toString())
+      txn2.commit()
+    } finally if (txn2 != null) txn2.close()
+
     env.close()
   }
 }
